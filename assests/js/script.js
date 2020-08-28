6286536e1226d06622b7a0897e3b3c33ea457e6a
rongbang <rongbang.ye@gmail.com>
2020-08-28 01:05:29 -0700

    refactor javascript and updated UI

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,19 +1,23 @@
 let cities = [];
 
+const api = {
+  key: "45706455bc2c4824123926c813998ea6",
+  base: "https://api.openweathermap.org/data/2.5/"
+};
+
 //  fetch data from weather API
 const fetchWeather = searchCityInput => {
-  console.log(searchCityInput);
-
   let API = "45706455bc2c4824123926c813998ea6";
 
-  // api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
-  let API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${searchCityInput}&units=imperial&appid=${API}`;
+  // Current Day Weather URL: api.openweathermap.org/data/2.5/weather?q={city name}&appid={your api key}
+  // let API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${searchCityInput}&units=imperial&appid=${API}`;
 
-  // api.openweathermap.org/data/2.5/forecast/daily?q={city name}&cnt={cnt}&appid={your api key}
-  let API_URL_FORCAST = `https://api.openweathermap.org/data/2.5/forecast?q=${searchCityInput}&units=imperial&appid=${API}`;
-  console.log(API_URL_FORCAST);
+  // 5 Day URL:  api.openweathermap.org/data/2.5/forecast/daily?q={city name}&cnt={cnt}&appid={your api key}
+  // let API_URL_FORCAST = `https://api.openweathermap.org/data/2.5/forecast?q=${searchCityInput}&units=imperial&appid=${API}`;
 
-  fetch(API_URL)
+  fetch(
+    `${api.base}weather?q=${searchCityInput}&units=imperial&appid=${api.key}`
+  )
     .then(response => {
       return response.json();
     })
@@ -21,9 +25,10 @@ const fetchWeather = searchCityInput => {
       let lon = data.coord.lon;
       let lat = data.coord.lat;
       // UV index: http://api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}
-      let API_URL_CurrentDay = `http://api.openweathermap.org/data/2.5/uvi?appid=${API}&lat=${lat}&lon=${lon}`;
+      // let API_URL_CurrentDay = `http://api.openweathermap.org/data/2.5/uvi?appid=${API}&lat=${lat}&lon=${lon}`;
 
-      fetch(API_URL_CurrentDay)
+      // Fetch UV index and date
+      fetch(`${api.base}uvi?appid=${api.key}&lat=${lat}&lon=${lon}`)
         .then(response => {
           return response.json();
         })
@@ -31,9 +36,12 @@ const fetchWeather = searchCityInput => {
           updateUVandDate(data);
         });
 
-      displayWeatherData(searchCityInput, data);
+      displayCurrentWeather(searchCityInput, data);
 
-      let dataDays = fetch(API_URL_FORCAST)
+      // Fetch 5 Days Forcast
+      fetch(
+        `${api.base}forecast?q=${searchCityInput}&units=imperial&appid=${api.key}`
+      )
         .then(response => {
           return response.json();
         })
@@ -42,43 +50,43 @@ const fetchWeather = searchCityInput => {
         });
     })
     .catch(() => {
-      console.log("Incorrect City");
+      alert("Please fill in a valid city");
     });
 };
 
+// Display UV and Current Date
 const updateUVandDate = data => {
   let now = moment.utc(data.date_iso).format("MM/DD/YYYY");
   let currentDate = document.querySelector(".currentDate");
   let currentUVIndex = document.querySelector(".currentUVIndex");
-  console.log(data);
+
   currentDate.innerHTML = `(${now})`;
   currentUVIndex.innerHTML = data.value;
 };
 
+// Display 5 Day Forcast
 const displayDaysForcast = dataDays => {
-  console.log(dataDays);
-
-  let dataDayOne = dataDays.list[0];
+  let dataDayOne = dataDays.list[5];
   let dayOne = document.querySelector(".dayOne");
   let dayOneTemp = document.querySelector(".dayOneTemp");
   let dayOneHumidity = document.querySelector(".dayOneHumidity");
 
-  let dataDayTwo = dataDays.list[6];
+  let dataDayTwo = dataDays.list[13];
   let dayTwo = document.querySelector(".dayTwo");
   let dayTwoTemp = document.querySelector(".dayTwoTemp");
   let dayTwoHumidity = document.querySelector(".dayTwoHumidity");
 
-  let dataDayThree = dataDays.list[14];
+  let dataDayThree = dataDays.list[21];
   let dayThree = document.querySelector(".dayThree");
   let dayThreeTemp = document.querySelector(".dayThreeTemp");
   let dayThreeHumidity = document.querySelector(".dayThreeHumidity");
 
-  let dataDayFour = dataDays.list[22];
+  let dataDayFour = dataDays.list[29];
   let dayFour = document.querySelector(".dayFour");
   let dayFourTemp = document.querySelector(".dayFourTemp");
   let dayFourHumidity = document.querySelector(".dayFourHumidity");
 
-  let dataDayFive = dataDays.list[30];
+  let dataDayFive = dataDays.list[37];
   let dayFive = document.querySelector(".dayFive");
   let dayFiveTemp = document.querySelector(".dayFiveTemp");
   let dayFiveHumidity = document.querySelector(".dayFiveHumidity");
@@ -104,32 +112,30 @@ const displayDaysForcast = dataDays => {
   dayFiveHumidity.innerHTML = dataDayFive.main.humidity;
 };
 
-const displayWeatherData = (searchCityInput, data) => {
+// Display the current Weather
+const displayCurrentWeather = (searchCityInput, data) => {
   let currentCityName = document.querySelector("#currentCityName");
-  let currentDate = document.querySelector(".currentDate");
   let currentTemperature = document.querySelector(".currentTemperature");
   let currentHumidity = document.querySelector(".currentHumidity");
   let currentWindSpeed = document.querySelector(".currentWindSpeed");
-  let currentUVIndex = document.querySelector(".currentUVIndex");
 
   if (searchCityInput) {
     currentCityName.innerHTML = data.name;
-    currentDate.innerHTML = "";
     currentTemperature.innerHTML = data.main.temp;
     currentHumidity.innerHTML = data.main.humidity;
     currentWindSpeed.innerHTML = data.wind.speed;
-    // currentUVIndex.innerHTML = current.main.temp;
   }
-
-  console.log(data);
 };
 
+// Display Search Cities History
 const renderMyCities = () => {
   document.querySelector("#searchCityInput").value = "";
   let searchCityList = document.querySelector(".main_searchCityList");
   let myCities = JSON.parse(localStorage.getItem("mySearchCities"));
   if (myCities !== null) {
     searchCityList.innerHTML = myCities
+      .slice(0)
+      .reverse()
       .map(city => {
         return `<h3>${city}</h3>`;
       })
@@ -138,11 +144,12 @@ const renderMyCities = () => {
 
   let currentCity = myCities.length;
   if (currentCity > 0) {
-    console.log(myCities[currentCity - 1]);
     fetchWeather(myCities[currentCity - 1]);
   }
 };
 
+// This function fires when click the search button or hit enter
+// Save the input into localStorage
 const searchWeather = event => {
   event.preventDefault();
   let searchCityInput = document.querySelector("#searchCityInput").value;
